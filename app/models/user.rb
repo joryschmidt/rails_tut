4a426f159fc7011f31c547093c109b2bc727f6c2
Jory Schmidt <joryschmidt1988@gmail.com>
2015-08-30 21:25:59 +0000

    forgetting a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,13 @@ class User < ActiveRecord::Base
     self.remember_token = User.new_token 
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+  
+  #returns true if given token matches digest
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
