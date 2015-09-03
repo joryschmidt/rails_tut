9a69b90a5743293d7ed24f5edb18ec886ab29e49
Jory Schmidt <joryschmidt1988@gmail.com>
2015-09-03 23:23:36 +0000

    authorization and friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -17,10 +20,38 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Profile successfully updated!'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:warning] = 'Please login'
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find_by(params[:id])
+      flash[:danger] = 'Naughty, naughty. You are only allowed to update your own profile.'
+      redirect_to root_path unless current_user?(@user)
     end
 end
