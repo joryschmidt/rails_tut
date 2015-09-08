ea783e9dd4b22588c5a45fdce8b4af2af63f0214
Jory Schmidt <joryschmidt1988@gmail.com>
2015-09-08 22:56:51 +0000

    password reset functionality

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+  
   def new
   end
 
@@ -6,7 +10,7 @@ class PasswordResetsController < ApplicationController
   end
   
   def create
-    @user = User.find_by(:email, params[:password_reset][:email])
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
       @user.send_password_email
@@ -19,5 +23,36 @@ class PasswordResetsController < ApplicationController
   end
   
   def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = 'Your password has been reset'
+      redirect_to @user
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+  
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+  
+  def valid_user
+    unless(@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
+  end
+  
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = 'Your reset link has expired'
+      redirect_to new_password_reset_url
+    end
   end
 end
